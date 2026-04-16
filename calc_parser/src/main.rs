c915013a6c9c44a7c324c29f014f0b81e4205397
Gus de Souza <gustavo.souza@avidbots.com>
2026-04-15 21:07:09 -0400

    implement calc_parser

--- calc_parser/src/main.rs
+++ calc_parser/src/main.rs
@@ -1,3 +1,56 @@
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+    let current_program_path = args.next().unwrap();
+    let source_path = args.next();
+    if source_path.is_none() {
+        eprintln!("Usage: {} <source_file>", current_program_path);
+    } else {
+        process_file(&current_program_path, &source_path.unwrap());
+    }
+} 
+
+fn process_file(current_program_path: &str, source_path: &str) {
+    const CALC_SUFFIX: &str = ".calc";
+    if !source_path.ends_with(CALC_SUFFIX) {
+        eprintln!("Error: Source file must have a '{}' extension.", CALC_SUFFIX);
+        return;
+    }
+    let source_code = std::fs::read_to_string(source_path);
+    if source_code.is_err() {
+        eprintln!(
+            "Error: Could not read source file {}: ({})", 
+            source_path,
+            source_code.err().unwrap()        
+        );
+        return;
+    }
+    let source_code = source_code.unwrap();
+
+    let parsed_program;
+    match parser::parse_program(&source_code) {
+        Ok((rest, syntax_tree)) => {
+            let trimmed_rest = rest.trim();
+            if trimmed_rest.len() > 0 {
+                eprintln!(
+                    "Invalid remaining code in '{}': {}",
+                    source_path, 
+                    trimmed_rest
+                );
+                return;
+            }
+            parsed_program = syntax_tree;
+        }
+        Err(error) => {
+            eprintln!(
+                "Error parsing source file {}: ({})", 
+                source_path,
+                error
+            );
+            return;
+        }
+    }
+
+    println!("Parsed program from '{}':\n{:#?}", source_path, parsed_program);
 }
---
+++ calc_parser/src/parser.rs
@@ -0,0 +1,145 @@
+extern crate nom;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    bytes::complete::take_while,
+    character::complete::{alpha1, char},
+    combinator::map,
+    multi::many0,
+    number::complete::double,
+    sequence::{delimited, preceded, tuple},
+    Parser,
+    IResult,
+};
+
+#[derive(Debug, PartialEq)]
+pub enum ParsedFactor<'a> {
+    Literal(f64),
+    Identifier(&'a str),
+    SubExpression(Box<ParsedExpr<'a>>),
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum TermOperator {
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum ExprOperator {
+    Add,
+    Subtract,
+}
+
+pub type ParsedTerm<'a> = (ParsedFactor<'a>, Vec<(TermOperator, ParsedFactor<'a>)>);
+
+pub type ParsedExpr<'a> = (ParsedTerm<'a>, Vec<(ExprOperator, ParsedTerm<'a>)>);
+
+#[derive(Debug)]
+pub enum ParsedStatement<'a> {
+    Declaration(&'a str),
+    InputOperation(&'a str),
+    OutputOperation(ParsedExpr<'a>),
+    Assignment(&'a str, ParsedExpr<'a>),
+}
+
+pub type ParsedProgram<'a> = Vec<ParsedStatement<'a>>;
+
+pub fn parse_program(input: &str) -> IResult<&str, ParsedProgram> {
+    many0(preceded(
+        skip_spaces,
+        alt((
+            parse_declaration,
+            parse_input_statement,
+            parse_output_statement,
+            parse_assignment,
+        )),
+    )).parse(input)
+}
+
+fn parse_declaration(input: &str) -> IResult<&str, ParsedStatement> {
+    (char('@'), skip_spaces, parse_identifier).parse(input)
+        .map(|(input, output)| (input, ParsedStatement::Declaration(output.2)))
+}
+
+fn parse_input_statement(input: &str) -> IResult<&str, ParsedStatement> {
+    (char('>'), skip_spaces, parse_identifier).parse(input)
+        .map(|(input, output)| (input, ParsedStatement::InputOperation(output.2)))
+}
+
+fn parse_output_statement(input: &str) -> IResult<&str, ParsedStatement> {
+    (char('<'), skip_spaces, parse_expr).parse(input)
+        .map(|(input, output)| (input, ParsedStatement::OutputOperation(output.2)))
+}
+
+fn parse_assignment(input:&str) -> IResult<&str, ParsedStatement> {
+    ( parse_identifier, 
+      skip_spaces, 
+      tag(":="),
+      skip_spaces,
+      parse_expr,
+    ).parse(input)
+    .map(|(input, output)| (input, ParsedStatement::Assignment(output.0, output.4)))
+}
+
+fn parse_identifier(input: &str) -> IResult<&str, &str> {
+    alpha1(input)
+}
+
+fn parse_subexpr(input: &str) -> IResult<&str, ParsedExpr> {
+    delimited(
+        preceded(skip_spaces, char('(')),
+        parse_expr,
+        preceded(skip_spaces, char(')')),
+    ).parse(input)
+}
+
+fn parse_factor(input: &str) -> IResult<&str, ParsedFactor> {
+    preceded(
+        skip_spaces,
+        alt((
+            map(parse_identifier, ParsedFactor::Identifier),
+            map(double, ParsedFactor::Literal),
+            map(parse_subexpr, |expr| {
+                ParsedFactor::SubExpression(Box::new(expr))
+            }),
+        )),
+    ).parse(input)
+}
+
+fn parse_term(input:&str) -> IResult<&str, ParsedTerm> {
+    (
+        parse_factor,
+        many0((
+            preceded(
+                skip_spaces,
+                alt((
+                    map(char('*'), |_| TermOperator::Multiply),
+                    map(char('/'), |_| TermOperator::Divide)
+                )),
+            ),
+            parse_factor,
+        )),
+    ).parse(input)
+}
+
+fn parse_expr(input: &str) -> IResult<&str, ParsedExpr> {
+    (
+        parse_term,
+        many0((
+            preceded(
+                skip_spaces,
+                alt((
+                    map(char('+'), |_| ExprOperator::Add),
+                    map(char('-'), |_| ExprOperator::Subtract)
+                )),
+            ),
+            parse_term,
+        )),
+    ).parse(input)
+}
+
+fn skip_spaces(input: &str) -> IResult<&str, &str> {
+    let chars = " \t\r\n";
+    take_while(move |ch| chars.contains(ch)).parse(input)
+}
